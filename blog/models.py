5f08b6d4491879fc9af058adb6dd7707e46f7915
ViolettaP <violetta.pukhova@mail.ru>
2023-09-22 21:30:22 +0300

    added blog

---
+++ blog/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+from django.urls import reverse
+
+from config import settings
+
+NULLABLE = {'blank': True, 'null': True}
+
+
+class Blog(models.Model):
+    """Запись в блоге"""
+    title = models.CharField(max_length=100, verbose_name='Заголовок')
+    body = models.TextField(verbose_name='Содержимое')
+    preview_pic = models.ImageField(upload_to='blog_pictures/', **NULLABLE, verbose_name='Изображение')
+    creation_date = models.DateField(auto_now_add=True, verbose_name='Дата создания')
+    is_published = models.BooleanField(default=True, verbose_name='Опубликовано')
+    views_count = models.PositiveIntegerField(default=0, verbose_name='Просмотры')
+    creator = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, **NULLABLE, verbose_name='Author')
+
+    def __str__(self):
+        return f'{self.title}'
+
+    class Meta:
+        verbose_name = 'Article'
+        verbose_name_plural = 'Articles'
---
+++ blog/views.py
@@ -0,0 +1,56 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy, reverse
+from django.views.generic import CreateView, DetailView, ListView, UpdateView, DeleteView
+
+from blog.forms import BlogForm
+from blog.models import Blog
+
+
+class BlogCreateView(LoginRequiredMixin, CreateView):
+    model = Blog
+    form_class = BlogForm
+    success_url = reverse_lazy('blog:list')
+
+    def form_valid(self, form):
+        self.object = form.save()
+        self.object.creator = self.request.user
+        self.object.save()
+        return super().form_valid(form)
+
+
+class BlogUpdateView(LoginRequiredMixin, UpdateView):
+    model = Blog
+    form_class = BlogForm
+
+    def form_valid(self, form):
+        self.object = form.save()
+        self.object.creator = self.request.user
+        self.object.save()
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse('blog:view', args=[self.kwargs.get('pk')])
+
+
+class BlogListView(ListView):
+    model = Blog
+
+    def get_queryset(self, *args, **kwargs):
+        queryset = super().get_queryset(*args, **kwargs)
+        queryset = queryset.filter(is_published=True)
+        return queryset
+
+
+class BlogDetailView(DetailView):
+    model = Blog
+
+    def get_object(self, queryset=None):
+        self.object = super().get_object(queryset)
+        self.object.views_count += 1
+        self.object.save()
+        return self.object
+
+
+class BlogDeleteView(LoginRequiredMixin, DeleteView):
+    model = Blog
+    success_url = reverse_lazy('blog:list')
